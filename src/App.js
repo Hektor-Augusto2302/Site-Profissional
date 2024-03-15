997c6ef2928f6de010c84657cb5880a6bea6736e
Hektor-Augusto <hektoraugusto.s@gmail.com>
2024-03-15 12:12:07 -0300

    feat(footer)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
@@ -21,6 +22,7 @@ function App() {
             <Route path='/contact' element={<Contact />} />
           </Routes>
         </div>
+        <Footer />
       </div>
     </BrowserRouter>
   );
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer id="footer">
+            <div className="container">
+                <div className="copyright">
+                    &copy; Copyright <strong><span>Hektor Augusto</span></strong>
+                </div>
+                <div className="credits">Desenvolvido por: Hektor Augusto</div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
